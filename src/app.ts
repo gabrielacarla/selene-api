440fde6849514486bf275917f425d7cb57b1497e
gabrielacarla <gabbiecarla@gmail.com>
2026-07-09 20:14:36 -0300

    refactor: move routes to dedicated router

--- src/app.ts
+++ src/app.ts
@@ -1,13 +1,10 @@
 import express from "express";
+import router from "./routes";
 
 const app = express();
 
 app.use(express.json());
 
-app.get("/", (req, res) => {
-  res.json({
-    message: "Selene API is running ⏾"
-  });
-});
+app.use(router);
 
 export default app;
